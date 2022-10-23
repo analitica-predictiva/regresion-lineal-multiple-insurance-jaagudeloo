c55628bb22ea9b631ae68e0d88250c158be9ce41
jaagudeloo <jaagudeloo@unal.edu.co>
2022-10-23 16:37:15 -0500

    Lab3_v5
    Lab3_v5

--- Ejercicio_3.py
+++ Ejercicio_3.py
@@ -119,6 +119,7 @@ def pregunta_03():
 
     # Defina un diccionario de parámetros para el GridSearchCV. Se deben
     # considerar valores desde 1 hasta 11 regresores para el modelo
+    # se deben revisar mayúsculas y minúsculas en el diccionario
     param_grid = {
         'selectKBest__k': np.arange(1,12,1),
     }
@@ -149,7 +150,7 @@ def pregunta_04():
     """
 
     # Importe mean_squared_error
-    from ____ import ____
+    from sklearn.metrics import mean_squared_error
 
     # Obtenga el pipeline optimo de la pregunta 3.
     gridSearchCV = pregunta_03()
@@ -158,20 +159,20 @@ def pregunta_04():
     X_train, X_test, y_train, y_test = pregunta_02()
 
     # Evalúe el modelo con los conjuntos de entrenamiento y prueba.
-    y_train_pred = ____.____(____)
-    y_test_pred = ____.____(____)
+    y_train_pred = gridSearchCV.predict(X_train)
+    y_test_pred = gridSearchCV.predict(X_test)
 
     # Compute el error cuadratico medio de entrenamiento y prueba. Redondee los
     # valores a dos decimales.
 
-    mse_train = ____(
-        _____,
-        _____,
+    mse_train = mean_squared_error(
+        y_train,
+        y_train_pred,
     ).round(2)
 
-    mse_test = ____(
-        _____,
-        _____,
+    mse_test = mean_squared_error(
+        y_test,
+        y_test_pred,
     ).round(2)
 
     # Retorne el error cuadrático medio para entrenamiento y prueba
